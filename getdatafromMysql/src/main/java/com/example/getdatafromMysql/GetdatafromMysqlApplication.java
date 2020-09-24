e41a6c8ec056e481626b64fa19783401f11d8208
Honghao Qiu <3158874848@qq.com>
2020-09-24 11:10:05 +0800

    access mysql data

---
+++ getdatafromMysql/src/main/java/com/example/getdatafromMysql/GetdatafromMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.getdatafrommysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetdatafrommysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetdatafrommysqlApplication.class, args);
+    }
+
+}
---
+++ getdatafromMysql/src/test/java/com/example/getdatafromMysql/GetdatafromMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getdatafrommysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetdatafrommysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

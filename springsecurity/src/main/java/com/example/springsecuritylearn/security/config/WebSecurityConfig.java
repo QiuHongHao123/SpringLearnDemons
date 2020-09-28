405a16e68d1e4a676e47813dff8c410119e3d9c8
Honghao Qiu <3158874848@qq.com>
2020-09-28 10:51:16 +0800

    login with spring security : add the remember me function

--- springsecurity/src/main/java/com/example/springsecuritylearn/security/config/WebSecurityConfig.java
+++ springsecurity/src/main/java/com/example/springsecuritylearn/security/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springsecuritylearn.security.config;
 
 import com.example.springsecuritylearn.security.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,10 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +22,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService userDetailsService;
+    //remember me 功能存储token进数据库然后对比
+    @Autowired
+    private DataSource dataSource;
+    //注入 dataSource
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(){
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        // 如果token表不存在，使用下面语句可以初始化该表；若存在，请注释掉这条语句，否则会报错。
+        tokenRepository.setCreateTableOnStartup(true);
+        return tokenRepository;
+    }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -48,7 +66,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .usernameParameter("username")
 //                .passwordParameter("password")
                 .and()
-                .logout().permitAll();
+                .logout().permitAll()
+                //使用Cookie进行自动登录
+                //这种直接存密码用户名进Cookie的方式虽然加密了但是不安全。
+                //Spring Security也支持其他rememberMe 的方式
+                //在客户端的 Cookie 中，仅保存一个无意义的加密串（与用户名、密码等敏感数据无关），
+                // 然后在数据库中保存该加密串-用户信息的对应关系，自动登录时，用 Cookie 中的加密串，到数据库中验证，如果通过，自动登录才算通过。
+                .and().rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                // 有效时间：单位s
+                .tokenValiditySeconds(60)
+                .userDetailsService(userDetailsService);;
 
         // 关闭CSRF跨域
         http.csrf().disable();
--- springsecurity/src/main/java/com/example/springsecuritylearn/security/service/CustomUserDetailsService.java
+++ springsecurity/src/main/java/com/example/springsecuritylearn/security/service/CustomUserDetailsService.java
@@ -48,4 +48,5 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 
     }
+
 }
